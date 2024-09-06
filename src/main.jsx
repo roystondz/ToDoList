a0ede82d488c19c44ab8f9148a8fbefb59dd24e5
roystondz <roystonad2004@gmail.com>
2024-09-06 22:36:11 +0530

    Added header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.render(<App/>,
+  document.getElementById("root")
+);
